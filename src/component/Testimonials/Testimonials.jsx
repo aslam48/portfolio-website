a8e1feb3c91320207bb029f63a913e7116b5e78a
aslam48 <youngfizzy001@gmail.com>
2022-08-21 22:40:10 +0100

    portfolio

---
+++ src/component/Testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Testimonials.css"
+
+
+let Testimonials = () => {
+  return (
+    <div>Testimonials</div>
+  )
+}
+export default Testimonials
---
+++ src/component/about/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./About.css"
+
+
+let About = () => {
+  return (
+    <div>about</div>
+  )
+}
+export default About
