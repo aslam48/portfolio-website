b8c4d4f58652f03b3a1fab06e816c3dcc6b8f462
aslam48 <youngfizzy001@gmail.com>
2022-08-27 17:09:47 +0100

    portfolio

--- src/component/Header/CTA.jsx
+++ src/component/Header/CTA.jsx
@@ -5,7 +5,7 @@ import CV from "../../assets/figma.pdf"
 export const CTA = () => {
   return (
     <div className='cta'>
-        <a href='https://digital-resume001.netlify.app/' className='btn btn-primary' >view CV</a>
+        <a href='https://digital-resume001.netlify.app/'  id='small' className='btn btn-primary'>view CV</a>
         <a href={CV} download className='btn'>Download CV </a>
         <a href='#contact' className='btn btn-primary'>Let TAlk</a>
     </div>
